14fabc2fb8336b8fe05204f7019c2106b96e3963
Admin <Admin@DESKTOP-34CAHMM>
2023-07-07 16:49:15 +0400

    Add project files.

---
+++ QueryArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using QueryArray;
+
+var arr = new[] { 1, 2, 3 };
+QueryArray<int> q = arr;
+QueryArray<int> q2 = new QueryArray<int>();
+q2.LoadFromArray(arr);
+q2.Add(4);
+q2.Add(5);
+
+//while (q.Next())
+//{
+//    Console.WriteLine(q.Current);
+//}
+//Console.WriteLine("----------");
+//while (q.Previous())
+//{
+//    Console.WriteLine(q.Current);
+//}
+//foreach (var item in q)
+//{
+//
+//   Console.WriteLine(item);
+//}
+foreach (var item in q2)
+{
+    Console.WriteLine(item);
+}
+//Console.WriteLine(q[1]);
---
+++ QueryArray/QueryArray.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryArray
+{
+    public class QueryArray<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] arr;
+        private int arrSize;
+        private const int defaultCapacity = 1_000;
+        private int currentIndex;
+        private bool eof;
+        private bool bof;
+        private int capacity => arr.Length;
+
+        public QueryArray(int capacity)
+        {
+            arr = new T[capacity];
+            Reset();
+        }
+
+        public QueryArray() : this(defaultCapacity)
+        {
+
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (currentIndex == -1)
+                    currentIndex = 0;
+                else if (currentIndex == arrSize)
+                    currentIndex = arrSize - 1;
+                return arr[currentIndex];
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public T this[int index] => arr[index];
+
+        public int Add(T item)
+        {
+            if (arrSize == capacity)
+                ReSize(arrSize * 2);
+            arr[arrSize] = item;
+            arrSize++;
+            return arrSize;
+        }
+
+        public int AddRange(T[] items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+            return arrSize;
+        }
+
+        private void ReSize(int newSize)
+        {
+            Array.Resize(ref arr, newSize);
+        }
+
+        public void Reset()
+        {
+            SetIndex(-1);
+        }
+
+        private void SetCurrent(int increase)
+        {
+            currentIndex += increase;
+            eof = currentIndex > 0 && currentIndex >= arrSize - 1;
+            bof = currentIndex == 0;
+        }
+
+        private void SetIndex(int index)
+        {
+            currentIndex = 0;
+            SetCurrent(index);
+        }
+
+        public bool Next()
+        {
+            if (!eof)
+            {
+                SetCurrent(1);
+                return true;
+            }
+            GotoEnd();
+            return false;
+        }
+
+        public bool Previous()
+        {
+            if (!bof)
+            {
+                SetCurrent(-1);
+                return true;
+            }
+            GotoStart();
+            return false;
+        }
+
+        public void GotoStart()
+        {
+            Reset();
+        }
+
+        public void GotoEnd()
+        {
+            SetIndex(arrSize);
+        }
+
+        public void LoadFromArray(T[] newArr)
+        {
+            Load(newArr);
+        }
+
+        private void Load(T[] newArr)
+        {
+            ArgumentNullException.ThrowIfNull(newArr);
+            arr = newArr;
+            arrSize = newArr.Length;
+            Reset();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            return Next();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(arr);
+        }
+
+        public static implicit operator QueryArray<T>(T[] newArr)
+        {
+            var q = new QueryArray<T>();
+            q.LoadFromArray(newArr);
+            return q;
+        }
+    }
+}
